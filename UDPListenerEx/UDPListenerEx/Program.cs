4ec35969e3c98ea3d1cd120b3373497920592515
Polenser <patrikfrederiksen@gmail.com>
2015-12-07 11:00:21 +0100

    Documentation

--- UDPListenerEx/UDPListenerEx/Program.cs
+++ UDPListenerEx/UDPListenerEx/Program.cs
@@ -28,36 +28,46 @@ namespace UDPListenerEx
             /// <param name="listenPort"> This parameter is used to choose on which port, that you want to listen on.</param>
             public static void StartListener(int listenPort)
             {
-                
+                // Done is used to run the while loop, checkweather is the weather from our third party webservice, and errorOccurred is used to check which method needs to be run
                 string checkWeather = "Error";
                 var errorOccurred = false;
                 bool done = false;
-                TempSoundService.Service1Client service = new Service1Client();
+
+                Service1Client service = new Service1Client();
                 UdpClient listener = new UdpClient(listenPort);
                 IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
-                WeatherService.GlobalWeatherSoapClient weather = new GlobalWeatherSoapClient();
+                GlobalWeatherSoapClient weather = new GlobalWeatherSoapClient();
                 try
                 {
                     while (!done)
                     {
-                        
+                        //Here we start our listener, and wait for a broadcast
                         Console.WriteLine("Waiting for broadcast");
                         byte[] bytes = listener.Receive(ref groupEP);
 
+                        //When we recieve the message, we then use GetString, and afterwards we split it
                         Console.WriteLine("Received broadcast from " +
-                            groupEP.ToString() + ": " +
+                            groupEP + ": " +
                             Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+
                         string message = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+
                         string[] response = message.Split(':');
+
+                        //In response we know where the temperature is, and we convert that to a double. The number we get is around 218, and because we need a value that looks like celcius, we divide it with 10 so we have an acceptable value
                         double temp = Convert.ToDouble(response[6].Trim().Substring(0, 3));
+
                         temp = temp/10;
                         Console.WriteLine(temp);
+
+                        //We need to send a Date and time plus soundlevel, which we randomly generate
                         string time = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy");
                         Console.WriteLine(time);
                         Random rand = new Random();
                         double sound = rand.Next(40, 60);
                         Console.WriteLine(sound);
                        
+                        //Here we get the weather for Roskilde, and afterwards we try to split the string that gets returned. But because the string we get can different depending on the weather, and exception can arise, and by catching that we can split it in another way, that then works
                         string todaysWeather = weather.GetWeather("Roskilde", "Denmark");
 
                         try
@@ -92,7 +102,7 @@ namespace UDPListenerEx
                     }
 
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                     string time = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy");
                     service.Datatransfer(time, 0, 0, "Error occurred while receiving broadcast");
@@ -109,12 +119,6 @@ namespace UDPListenerEx
         {
             
             UDPListener.StartListener(7000);
-            
-          
-            
-            
-            
-
         }
         
 
